abbedceffe21e4850721905504f92c310473df38
WAYN3x <179059483+WAYN3x@users.noreply.github.com>
2024-12-18 16:35:13 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "e3IkgA5a#j-4oR3uh8HgxhDRH5nJx9FQte2L4GDbvs68P2hO4kCY", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "fy4CXICD#9CKbOjYSxWSGxKafP1O6VlExp7r4ElEUr8i-FHcxJiQ", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_STICKER: process.env.AUTO_STICKER || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 AUTO_REACT: process.env.AUTO_REACT || "false",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",    
